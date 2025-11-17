62d04df14b297dbcc154f34e86a473e3bafe9126
祐太 森 <morisam.yutakun@gmail.com>
2025-11-17 11:54:30 +0900

    Add policy_for_line template and update app routes

--- app.py
+++ app.py
@@ -19,10 +19,21 @@ def pricing():
 def policy():
     return render_template("policy.html")
 
+# 質問・添削ページ
+@app.route("/qanda")
+def qanda():
+    return render_template("qanda.html")
+
 # ビジネスラインへ飛ぶリンク
 @app.route("/business-line")
 def business_line():
     return redirect("https://lin.ee/VNppGay")  # 実際のURLに置き換え
+
+
+@app.route("/policy-for-line")
+def policy_for_line():
+    return render_template("policy_for_line.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
